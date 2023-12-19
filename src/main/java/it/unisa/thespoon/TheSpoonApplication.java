0f281d29eb435b7ab0b0a67f876c6bb0d5f8dc01
Jacopo Gennaro Esposito <37371071+jacopoesposito@users.noreply.github.com>
2023-12-19 13:51:25 +0100

    Configured Spring Boot
    Configured Spring Boot to start development

---
+++ src/main/java/it/unisa/thespoon/TheSpoonApplication.java
@@ -0,0 +1,14 @@
+package it.unisa.thespoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class TheSpoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSpoonApplication.class, args);
+	}
+
+}
